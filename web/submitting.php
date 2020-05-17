94b37a9de7a7ac6768ed8ebdaf80ad2ef714f601
ray314 <yeungj1602@gmail.com>
2020-05-17 15:01:39 +1200

    Update submitting.php
    removed port

--- web/submitting.php
+++ web/submitting.php
@@ -1,6 +1,6 @@
 <?php 
 
-//if (isset($_POST['name']) && isset($_POST['link']) && isset($_POST['date'])) {
+if (isset($_POST['name']) && isset($_POST['link']) && isset($_POST['date'])) {
     // Obtain all date
     $name = $_POST['name'];
     $link = $_POST['link'];
@@ -11,16 +11,15 @@
     $dbname = "d7n9b1n1v1sh20";
     $username = "ujebccinfaqukt";
     $password = "5bc5f1199e72b3a45bcd320fad11fcd5464ac989651150d10681d498fa65ff08";
-    $port = "5432";
     
     // Create SQLi object
     $mysqli = new mysqli($host, $username, $password, $dbname);
-    /*
+    
     if ($mysqli->connect_error) {
         echo "<p style='color:red;'>Connection to database failed. Error number: $mysqli->connect_errno</p>";
         die("Connection failed");
-    }*/
+    }
     echo "<p style='color:green;'>Successful</p>";
-//}
+}
 
 ?>
