f1a07f8ad3fa346f1030279ae7b521d6fd7e5320
ray314 <yeungj1602@gmail.com>
2020-05-17 14:47:15 +1200

    Update submitting.php
    creating connection to database

--- web/submitting.php
+++ web/submitting.php
@@ -1,7 +1,25 @@
 <?php 
 
-if (isset($_POST['name'])) {
+if (isset($_POST['name']) && isset($_POST['link']) && isset($_POST['date'])) {
+    // Obtain all date
     $name = $_POST['name'];
+    $link = $_POST['link'];
+    $date = $_POST['date'];
+
+    // Heroku Postgres credentials
+    $host = "ec2-52-71-55-81.compute-1.amazonaws.com";
+    $dbname = "d7n9b1n1v1sh20";
+    $username = "ujebccinfaqukt";
+    $password = "5bc5f1199e72b3a45bcd320fad11fcd5464ac989651150d10681d498fa65ff08";
+    $port = "5432";
+
+    // Create SQLi object
+    $mysqli = new mysqli($host, $username, $password, $dbname, $port);
+
+    if ($mysqli->connect_error) {
+        echo "<p style='color:red;'>Connection to database failed</p>";
+        die("Connection failed");
+    }
     echo "<p style='color:green;'>Successful</p>";
 }
 
