9816a519decf0e067eb859f3c0a7e100ea774adb
ray314 <yeungj1602@gmail.com>
2020-05-18 19:15:11 +1200

    Update submitting.php
    testing sql query

--- web/submitting.php
+++ web/submitting.php
@@ -7,10 +7,10 @@ if (isset($_POST['name']) && isset($_POST['link']) && isset($_POST['date'])) {
     $date = $_POST['date'];
 
     // Heroku Postgres credentials
-    $host = "ec2-52-71-55-81.compute-1.amazonaws.com";
-    $dbname = "d7n9b1n1v1sh20test";
-    $username = "ujebccinfaqukttest";
-    $password = "5bc5f1199e72b3a45bcd320fad11fcd5464ac989651150d10681d498fa65ff08";
+    //$host = "ec2-52-71-55-81.compute-1.amazonaws.com";
+    //$dbname = "d7n9b1n1v1sh20test";
+    //$username = "ujebccinfaqukttest";
+    //$password = "5bc5f1199e72b3a45bcd320fad11fcd5464ac989651150d10681d498fa65ff08";
     
     // Connect using pgsql
     $conn = pg_connect(getenv("DATABASE_URL"));
@@ -20,6 +20,10 @@ if (isset($_POST['name']) && isset($_POST['link']) && isset($_POST['date'])) {
         die();
     }
     echo "<p style='color:green;'>Successful</p>";
+
+    $sql = "CREATE TABLE testing (
+            ID INT PRIMARY KEY)";
+    pg_query($conn, $sql);
 }
 
 ?>
