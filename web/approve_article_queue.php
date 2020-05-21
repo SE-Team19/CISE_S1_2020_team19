d20deac64339ec0f3d003f1657d9af2a3b3996c1
jusyeung <yeungj1602@gmail.com>
2020-05-21 17:02:55 +1200

    added bibtex implementation

--- web/approve_article_queue.php
+++ web/approve_article_queue.php
@@ -14,6 +14,9 @@
 
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+        <!-- BibTEX js -->
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/pcooksey/bibtex-js/src/bibtex_js.js"></script>
         <title>Approve article</title>
     </head>
 
@@ -30,6 +33,21 @@
             
             
             <h5>To be approved</h5>
+
+            <bibtex src="test.bib"></bibtex>
+            <div id="bibtext_display"></div>
+
+            <p>
+                Title: Handbook of Mathematical Functions with
+              Formulas, Graphs, and Mathematical Tables <br>
+              Author: Milton Abramowitz and Irene A. Stegun <br>
+              Publisher: Dover <br>
+              Year: 1964 <br>
+              Address: New York City <br>
+              Edition: ninth Dover printing, tenth GPO printing <br>
+            </p>
+
+            <button id="approve" class="btn btn-primary mt-2" onclick
 
             <?php
             // Connect using pgsql
@@ -55,8 +73,6 @@
             //        Date: $date<br></p>";
             pg_close($conn);
             ?>
-            
-            <embed src="test.pdf" width="720px" height="480px">
 
         </div>
 
