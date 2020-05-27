6876b61b7aa73a64dfc14cfacac7b17d0868181e
ray314 <yeungj1602@gmail.com>
2020-05-28 11:51:58 +1200

    fixing

--- web/js/sortTable.js
+++ web/js/sortTable.js
@@ -26,32 +26,32 @@ function sortTable(id ,n) {
             // If so, mark as a switch and break the loop:
             shouldSwitch = true;
             // Change the arrow
-            document.getElementById(id).className = "arrow down";
+            dir = "arrow down";
             break;
           }
         } else if (dir == "arrow down") {
           if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
             // If so, mark as a switch and break the loop:
             shouldSwitch = true;
-            document.getElementById(id).className = "arrow up";
+            dir = "arrow up";
             break;
           }
         }
       }
-      /*if (shouldSwitch) {
-         If a switch has been marked, make the switch
-        and mark that a switch has been done: 
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
         // Each time a switch is done, increase this count by 1:
         switchcount ++;
       } else {
-         If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. 
-        if (switchcount == 0 && dir == "desc") {
-          dir = "asc";
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "arrow up") {
+          dir = "arrow down";
           switching = true;
         } 
-      }*/
+      }
     }
   }
