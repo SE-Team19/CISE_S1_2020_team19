b1af7d0a878b886e69a02cb7a57b04dfb4eb15fd
ray314 <yeungj1602@gmail.com>
2020-05-28 11:22:30 +1200

    debug

--- web/js/sortTable.js
+++ web/js/sortTable.js
@@ -48,8 +48,8 @@ function sortTable(id ,n) {
       } else {
         /* If no switching has been done AND the direction is "asc",
         set the direction to "desc" and run the while loop again. */
-        if (switchcount == 0 && dir == "asc") {
-          dir = "desc";
+        if (switchcount == 0 && dir == "desc") {
+          dir = "asc";
           switching = true;
         }
       }
