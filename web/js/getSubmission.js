6f7f6258a871396878ea0c3ea148fae216fb62be
ray314 <yeungj1602@gmail.com>
2020-06-02 11:39:46 +1200

    making it display details

--- web/js/getSubmission.js
+++ web/js/getSubmission.js
@@ -2,13 +2,18 @@ function getSubmission() {
     var xhttp = new XMLHttpRequest(); // Create XHR object
     var title = document.getElementsByTagName("title");
     var author = document.getElementsByTagName("author");
-    var year = document.getElementsByTagName("year");
+    var date = document.getElementsByTagName("year");
     var doi = document.getElementsByTagName("doi");
+    var description = document.getElementsByTagName("description");
     xhttp.open("GET", "moderator_get_submission.php"); // GET is fine for retrieving
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
-            console.log(obj);
+            title.value = obj.title;
+            author.value = obj.author;
+            date.value = obj.date;
+            doi.value = obj.doi;
+            description.value = obj.description;
         }
     }
     xhttp.send();
