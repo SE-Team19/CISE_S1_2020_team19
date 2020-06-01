927a2b7687782411f58c949155dc5282445e19f6
ray314 <yeungj1602@gmail.com>
2020-06-02 11:52:13 +1200

    updated getsubmission

--- web/js/getSubmission.js
+++ web/js/getSubmission.js
@@ -9,13 +9,26 @@ function getSubmission() {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
+            // Remove the readonly attribute to make the value editable
+            title.removeAttribute("readonly");
+            author.removeAttribute("readonly");
+            date.removeAttribute("readonly");
+            doi.removeAttribute("readonly");
+            description.removeAttribute("readonly");
+            // Insert the details
             title.value = obj.title;
             author.value = obj.author;
             date.value = obj.date;
             doi.value = obj.doi;
             description.value = obj.description;
             console.log(obj.title);
-        }
+            // Add readonly attribute back
+            title.setAttribute("readonly");
+            author.setAttribute("readonly");
+            date.setAttribute("readonly");
+            doi.setAttribute("readonly");
+            description.setAttribute("readonly");
+            }
     }
     xhttp.send();
 }
