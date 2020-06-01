ea5776206297b12c35e6bd006595dee1caebf907
ray314 <yeungj1602@gmail.com>
2020-06-02 11:35:05 +1200

    fixing bugs

--- web/js/getSubmission.js
+++ web/js/getSubmission.js
@@ -4,7 +4,7 @@ function getSubmission() {
     var author = document.getElementsByTagName("author");
     var year = document.getElementsByTagName("year");
     var doi = document.getElementsByTagName("doi");
-    xhttp.open("GET", "moderator.php"); // GET is fine for retrieving
+    xhttp.open("GET", "moderator_get_submission.php"); // GET is fine for retrieving
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
