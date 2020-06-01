bbc494ef06ee422233d55d44b341e554e926f387
ray314 <yeungj1602@gmail.com>
2020-06-02 11:55:06 +1200

    added index 0 to tagname

--- web/js/getSubmission.js
+++ web/js/getSubmission.js
@@ -1,20 +1,20 @@
 function getSubmission() {
     var xhttp = new XMLHttpRequest(); // Create XHR object
-    var title = document.getElementsByTagName("title");
-    var author = document.getElementsByTagName("author");
-    var date = document.getElementsByTagName("year");
-    var doi = document.getElementsByTagName("doi");
-    var description = document.getElementsByTagName("description");
+    var title = document.getElementsByTagName("title")[0];
+    var author = document.getElementsByTagName("author")[0];
+    var date = document.getElementsByTagName("year")[0];
+    var doi = document.getElementsByTagName("doi")[0];
+    var description = document.getElementsByTagName("description")[0];
     xhttp.open("GET", "moderator_get_submission.php"); // GET is fine for retrieving
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
             // Remove the readonly attribute to make the value editable
-            title.removeAttribute("readonly");
-            author.removeAttribute("readonly");
-            date.removeAttribute("readonly");
-            doi.removeAttribute("readonly");
-            description.removeAttribute("readonly");
+            //title.removeAttribute("readonly");
+            //author.removeAttribute("readonly");
+            //date.removeAttribute("readonly");
+            //doi.removeAttribute("readonly");
+            //description.removeAttribute("readonly");
             // Insert the details
             title.value = obj.title;
             author.value = obj.author;
@@ -23,11 +23,11 @@ function getSubmission() {
             description.value = obj.description;
             console.log(obj.title);
             // Add readonly attribute back
-            title.setAttribute("readonly");
-            author.setAttribute("readonly");
-            date.setAttribute("readonly");
-            doi.setAttribute("readonly");
-            description.setAttribute("readonly");
+            //title.setAttribute("readonly");
+            //author.setAttribute("readonly");
+            //date.setAttribute("readonly");
+            //doi.setAttribute("readonly");
+            //description.setAttribute("readonly");
             }
     }
     xhttp.send();
