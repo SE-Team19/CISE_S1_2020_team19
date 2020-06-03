8cc5dbaaf8b9a7e87d677d00f42a75577e0e7e14
ray314 <yeungj1602@gmail.com>
2020-06-04 11:55:48 +1200

    try failing the test

--- tests/Test.php
+++ tests/Test.php
@@ -10,11 +10,11 @@ final class EmailTest extends TestCase
             'user@example.com'
         );
     }
-
+    // Test if it can connect to PostgreSQL
     public function testConnectToDatabase(): void {
         $conn = pg_connect("host=ec2-52-71-55-81.compute-1.amazonaws.com
                             dbname=d7n9b1n1v1sh20
-                            port=5432
+                            port=5432s
                             user=ujebccinfaqukt
                             password=5bc5f1199e72b3a45bcd320fad11fcd5464ac989651150d10681d498fa65ff08
                             sslmode=require");
