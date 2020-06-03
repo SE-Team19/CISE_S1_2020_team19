667170280bc01a7a33c505b37fe5036f3e0dcbde
ray314 <yeungj1602@gmail.com>
2020-06-04 11:45:52 +1200

    unit test

--- tests/Test.php
+++ tests/Test.php
@@ -12,7 +12,12 @@ final class EmailTest extends TestCase
     }
 
     public function testConnectToDatabase(): void {
-        $conn = pg_connect(getenv("DATABASE_URL"));
+        $conn = pg_connect("host=ec2-52-71-55-81.compute-1.amazonaws.com
+                            dbname=d7n9b1n1v1sh20
+                            port=5432
+                            user=ujebccinfaqukt
+                            password=5bc5f1199e72b3a45bcd320fad11fcd5464ac989651150d10681d498fa65ff08
+                            sslmode=require");
         $this->assertTrue($conn);
     }
 }
