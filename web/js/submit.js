b446a8e4c29debb5762cf3cab3fa31dbaec1627b
ray314 <yeungj1602@gmail.com>
2020-06-05 09:56:20 +1200

    making submit process work

--- web/js/submit.js
+++ web/js/submit.js
@@ -4,6 +4,8 @@
  */
 function submit(e) {
     e.preventDefault();
+    // Create XHR object
+    var xhttp = new XMLHttpRequest();
     var name = form.getElementsByTagName("input")[0].value;
     var email = form.getElementsByTagName("input")[1].value;
     // Submission fields
@@ -21,7 +23,69 @@ function submit(e) {
                 "doi":doi,
                 "description":description }
     var json = JSON.stringify(obj);
-    console.log(json);
+
+    xhttp.open("POST", "submitprocess.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // Replace container elements with a successful message
+            container.innerHTML = this.responseText;
+        }
+    }
+
+    xhttp.send("json="+json);
+}
+
+/**
+ * Creates a new form
+ */
+function newArticle() {
+    container.innerHTML = `<!-- Submitter details (initial page) -->
+    <h3 class="hr_title"> New Article</h3>
+    <hr>
+    <!-- send form to self for now: in future, send to submitprocess.php -->
+    <form id="seer-form">
+            Name:<input class="namebar" type="text" name="name" 
+            pattern="[a-zA-Z]{1,}" title="Name must only contains only letters" required>
+                <br>
+            Email: <input class="emailbar" type="text" name="email" 
+            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Email must be email@something.something" required>
+        <br>
+        <br>
+        <div id="analyst_journal_table_container">
+        <div id="analyst_journal_container">
+            <div id="analyst_journal_left_container">
+                <div id="analyst_journal_info_container">
+                    <label class="analyst_label">Title: </label>
+                        <br>
+                    <input type="text" name="title" class="analyst_input" value="" >
+                        <br>
+                    <label class="analyst_label">Author: </label>
+                        <br>
+                    <input type="text" name="author" class="analyst_input" value="">
+                        <br>
+                    <label class="analyst_label">Date: </label>
+                        <br>
+                    <input type="text" name="year" class="analyst_input" value="">
+                        <br>
+                    <label class="analyst_label">DOI: </label>
+                        <br>
+                    <input type="text" name="doi" class="analyst_input" value="">
+                        <br>
+                </div>
+            </div>
+            <div id="analyst_journal_right_container">
+            <label class="analyst_label" id="analyst_journal_label">Description</label>
+            <br>
+            <textarea name="description" rows="9" cols="48" class="analyst_input">
+            </textarea>    
+            </div>
+        </div>
+        <input type="submit" id="analyst_button">
+        <button id="analyst_button" onclick="reset()">Reset</button>
+        <br>
+    </div>
+    </form>`;
 }
 
 function reset() {
@@ -34,6 +98,8 @@ function reset() {
     form.getElementsByTagName("input")[5].value = "";
     form.getElementsByTagName("textarea")[0].value = "";
 }
-
+// container and form variables
+var container = document.getElementById("form-container");
 var form = document.getElementById("seer-form");
+// Add event listener to prevent form from being submitted
 form.addEventListener("submit", submit);
--- web/submitprocess.php
+++ web/submitprocess.php
@@ -1,2 +1,28 @@
 <?php
+// Decode json into object
+$obj = json_decode($_POST["json"]);
+$name = $obj->name;
+$email = $obj->email;
+
+echo "<h3 class='hr_title'> Submit Article</h3>
+        <hr>
+        <br>
+        Thank you, $name!
+        <br>
+        <br>   
+        Your submission has been received and is now in Stage 2.
+        <br>
+        <br>
+        <br>
+        <!-- image of steps (s1: submit > s2: moderation > posted to SEER) -->
+        Stage 1: Submit ---> Stage 2: Moderation ---> Stage 3: Posted to SEER
+        <br>
+        <br>
+        <br>
+        A confirmation email has been sent to $email.
+        <br>
+        <br>
+        <button class='button' onclick='window.location.href='searchform.html''>Search</button>
+        <button class='button' onclick='newArticle()'>New Article</button>"
+
 ?>
