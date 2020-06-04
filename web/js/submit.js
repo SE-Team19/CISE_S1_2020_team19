b6f4da3ec232113782d23e95802b5be8abec9b28
ray314 <yeungj1602@gmail.com>
2020-06-04 14:10:39 +1200

    working on submit form page

---
+++ web/js/submit.js
@@ -0,0 +1,10 @@
+/**
+ * Submit the form for review
+ * @param {*} e - Event object
+ */
+function submit(e) {
+    e.preventDefault();
+}
+
+var form = document.getElementById("seer-form");
+form.addEventListener("submit", submit);
