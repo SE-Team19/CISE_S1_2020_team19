fbf5fee1ea7a05936f8bd2b6c45ed9b228e8a815
ray314 <yeungj1602@gmail.com>
2020-06-04 23:39:04 +1200

    fixing json

--- web/js/submit.js
+++ web/js/submit.js
@@ -4,14 +4,14 @@
  */
 function submit(e) {
     e.preventDefault();
-    var name = form.getElementsByTagName("input")[0];
-    var email = form.getElementsByTagName("input")[1];
+    var name = form.getElementsByTagName("input")[0].value;
+    var email = form.getElementsByTagName("input")[1].value;
     // Submission fields
-    var title = form.getElementsByTagName("input")[2];
-    var author = form.getElementsByTagName("input")[3];
-    var date = form.getElementsByTagName("input")[4];
-    var doi = form.getElementsByTagName("input")[5];
-    var description = form.getElementsByTagName("textarea")[0];
+    var title = form.getElementsByTagName("input")[2].value;
+    var author = form.getElementsByTagName("input")[3].value;
+    var date = form.getElementsByTagName("input")[4].value;
+    var doi = form.getElementsByTagName("input")[5].value;
+    var description = form.getElementsByTagName("textarea")[0].value;
     // Convert to JSON
     var obj = { "name":name,
                 "email":email,
@@ -22,7 +22,6 @@ function submit(e) {
                 "description":description }
     var json = JSON.stringify(obj);
     console.log(json);
-    console.log(name);
 }
 
 function reset() {
