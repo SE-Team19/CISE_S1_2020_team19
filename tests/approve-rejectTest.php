c920da10e394d6988bf6752b790fd196f3334664
ray314 <yeungj1602@gmail.com>
2020-06-10 10:39:10 +1200

    created unit test for reject

--- tests/approve-rejectTest.php
+++ tests/approve-rejectTest.php
@@ -27,11 +27,6 @@ function approve($submit, $status) {
                 SET status = 'rejected'
                 WHERE title ILIKE '$submit'";
         $result = pg_query($sql);
-        if ($result == false) {
-            echo "<h3 style='color:red;'>Error rejected submission: " + pg_last_error() + "</h3>";
-        } else {
-            echo "<h3 style='color:green;'>Successfully rejected" + "</h3>";
-        } 
     }
 
 final class ApproveRejectTestCase extends TestCase {
@@ -61,6 +56,38 @@ final class ApproveRejectTestCase extends TestCase {
 
 
         $this->assertEquals("processing", $actual['status']);
+
+        // Clean up
+        $sql = "DELETE FROM articles
+                WHERE title = 'TEST'";
+        pg_query($sql);
+    }
+
+    function testReject(): void {
+        $conn = pg_connect("host=ec2-52-71-55-81.compute-1.amazonaws.com
+                            dbname=d7n9b1n1v1sh20
+                            port=5432
+                            user=ujebccinfaqukt
+                            password=5bc5f1199e72b3a45bcd320fad11fcd5464ac989651150d10681d498fa65ff08
+                            sslmode=require");
+        $sql = "INSERT INTO articles (title, author, status)
+                VALUES ('TEST', 'TEST', 'pending review')";
+        pg_query($sql);
+
+        $sql = "UPDATE articles
+                SET status = 'pending review'
+                WHERE title = 'TEST'";
+        pg_query($sql);
+
+        reject("TEST", "pending review");
+
+        $sql = "SELECT status from articles
+                WHERE title = 'TEST'";
+        $result = pg_query($sql);
+        $actual = pg_fetch_assoc($result);
+
+
+        $this->assertEquals("rejected", $actual['status']);
 
         // Clean up
         $sql = "DELETE FROM articles
