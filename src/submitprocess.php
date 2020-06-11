ef645f6cd4583bec67f56dcee0869b2c1c90f4be
ray314 <yeungj1602@gmail.com>
2020-06-11 19:43:39 +1200

    making it better

--- src/submitprocess.php
+++ src/submitprocess.php
@@ -19,34 +19,36 @@ if (isset($_POST["json"])) {
 function submit($title, $author, $date, $doi, $description) {
     $sql = "INSERT INTO articles (title, author, date, doi, description)
             VALUES ('$title', '$author', '$date', '$doi', '$description')";
+    
     $result = pg_query($sql);
 
-    if (pg_num_rows($result) > 0) {
-        if (!isset($name) || !isset($email)) {
-            $name = "Test";
-            $email = "Test";
+    if ($result) {
+        if (!isset($name) && !isset($email)) {
+            echo "Successful insert";
+        } else {
+            echo "<h3 class='hr_title'> Submit Article</h3>
+            <hr>
+            <br>
+            Thank you, $name!
+            <br>
+            <br>   
+            Your submission has been received and is now in Stage 2.
+            <br>
+            <br>
+            <br>
+            Stage 1: Submit ---> Stage 2: Moderation ---> Stage 3: Posted to SEER
+            <br>
+            <br>
+            <br>
+            A confirmation email has been sent to $email.
+            <br>
+            <br>
+            <button class='button' onclick='window.location.href='searchform.html''>Search</button>
+            <button class='button' onclick='newArticle()'>New Article</button>";
         }
 
-        echo "<h3 class='hr_title'> Submit Article</h3>
-        <hr>
-        <br>
-        Thank you, $name!
-        <br>
-        <br>   
-        Your submission has been received and is now in Stage 2.
-        <br>
-        <br>
-        <br>
-        Stage 1: Submit ---> Stage 2: Moderation ---> Stage 3: Posted to SEER
-        <br>
-        <br>
-        <br>
-        A confirmation email has been sent to $email.
-        <br>
-        <br>
-        <button class='button' onclick='window.location.href='searchform.html''>Search</button>
-        <button class='button' onclick='newArticle()'>New Article</button>";
-    } else if (pg_num_rows($result) == 0) {
+        
+    } else {
         echo "<h3 class='hr_title'> Submit Article</h3>
         <hr>
         <br>
