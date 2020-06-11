03aa9432e7db7809fd78b67296f5731b9e10ecda
ray314 <yeungj1602@gmail.com>
2020-06-11 19:50:24 +1200

    updated

--- src/submitprocess.php
+++ src/submitprocess.php
@@ -11,43 +11,36 @@ if (isset($_POST["json"])) {
     $date = $obj->date;
     $doi = $obj->doi;
     $description = $obj->description;
-    submit($title, $author, $date, $doi, $description);
+    submit($name, $email, $title, $author, $date, $doi, $description);
 }
 /**
  * Submit the details
  */
-function submit($title, $author, $date, $doi, $description) {
+function submit($name, $email, $title, $author, $date, $doi, $description) {
     $sql = "INSERT INTO articles (title, author, date, doi, description)
             VALUES ('$title', '$author', '$date', '$doi', '$description')";
-    
     $result = pg_query($sql);
 
     if ($result) {
-        if (!isset($name) && !isset($email)) {
-            echo "Successful insert";
-        } else {
-            echo "<h3 class='hr_title'> Submit Article</h3>
-            <hr>
-            <br>
-            Thank you, $name!
-            <br>
-            <br>   
-            Your submission has been received and is now in Stage 2.
-            <br>
-            <br>
-            <br>
-            Stage 1: Submit ---> Stage 2: Moderation ---> Stage 3: Posted to SEER
-            <br>
-            <br>
-            <br>
-            A confirmation email has been sent to $email.
-            <br>
-            <br>
-            <button class='button' onclick='window.location.href='searchform.html''>Search</button>
-            <button class='button' onclick='newArticle()'>New Article</button>";
-        }
-
-        
+        echo "<h3 class='hr_title'> Submit Article</h3>
+        <hr>
+        <br>
+        Thank you, $name!
+        <br>
+        <br>   
+        Your submission has been received and is now in Stage 2.
+        <br>
+        <br>
+        <br>
+        Stage 1: Submit ---> Stage 2: Moderation ---> Stage 3: Posted to SEER
+        <br>
+        <br>
+        <br>
+        A confirmation email has been sent to $email.
+        <br>
+        <br>
+        <button class='button' onclick='window.location.href='searchform.html''>Search</button>
+        <button class='button' onclick='newArticle()'>New Article</button>";        
     } else {
         echo "<h3 class='hr_title'> Submit Article</h3>
         <hr>
